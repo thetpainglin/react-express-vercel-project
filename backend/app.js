d20b44870a43e62884228e19f08accb0acf1a85d
thetpainglin <76717297+thetpainglin@users.noreply.github.com>
2024-02-11 02:18:18 +0630

    app.js

--- backend/app.js
+++ backend/app.js
@@ -56,7 +56,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors(
 {
       origin : ["https://deploying-express-api.vercel.app/api/products","https://deploying-express-api.vercel.app/api/banner"],
-      method : ["POST","GET","PUT","PATCH"],
+      methods : ["POST","GET","PUT","PATCH"],
       credentials : true
     }
 ));
