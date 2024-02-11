25d95bb69e2ede53062587f3ea249058267c460f
thetpainglin <76717297+thetpainglin@users.noreply.github.com>
2024-02-11 10:03:49 +0630

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -35,7 +35,7 @@ var app = express();
 
 app.use(cors(
 {
-      origin: ["https://deploying-express-api.vercel.app/api/*"],
+      origin: "https://deploying-express-api.vercel.app/api/*",
       methods: ["POST","GET","PUT","PATCH","DELETE"]
     }
 ));
