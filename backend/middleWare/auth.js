1d6c7e459539c0bf4f5548d137f5ae657c2f9712
thetpainglin <76717297+thetpainglin@users.noreply.github.com>
2024-02-11 11:11:56 +0630

    Update auth.js

--- backend/middleWare/auth.js
+++ backend/middleWare/auth.js
@@ -2,16 +2,6 @@ let jwt = require('jsonwebtoken');
 const {config} = require("../config/Config");
 
 const verifyUserToken = (req,res,next)=>{
-
-    res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
     
     let token = req.headers.authorization;
     if(!token) return res.status(401).send("Access Denied/ Unauthorized request");
